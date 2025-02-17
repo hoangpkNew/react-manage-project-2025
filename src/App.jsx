5638c406d25693d36b092a253095a37fa72f966a
hoangdv.hp <hoangdv.hp99@gmail.com>
2025-02-18 00:02:53 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+}
+
+export default App;
